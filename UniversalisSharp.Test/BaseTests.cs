1eca981f118c6f4b6bf2987ecde20ae1d0734b29
Alexew <gauba.alexew@gmail.com>
2022-09-05 16:42:02 +0300

    Create project for tests

---
+++ UniversalisSharp.Test/BaseTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalisSharp.Test
+{
+	public class BaseTests
+	{
+		public readonly UniversalisClient client;
+
+		public const string DataCenter = "Chaos";
+		public const int ItemId = 37775;
+		public const int ItemId2 = 37776;
+
+		public BaseTests()
+		{
+			client = new UniversalisClient();
+		}
+	}
+}
---
+++ UniversalisSharp.Test/EndpointTests/MarketTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UniversalisSharp.Test.EndpointTests
+{
+	[TestClass]
+	public class MarketTests : BaseTests
+	{
+		[TestMethod]
+		public void GetItemsDataAsync()
+		{
+			var items = client.Market.GetItemsDataAsync(DataCenter, new int[] { ItemId, ItemId2 }).Result;
+
+			Assert.IsTrue(items.Count > 0);
+		}
+	}
+}
